15470bfca54d8564d71ee98d6fee8a62df918d92
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-29 19:08:38 +0200

    input field finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div>
+      <section className="add">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => {
+            e.preventDefault();
+            setInputValue(e.target.value);
+            console.log(inputValue);
+          }}
+        />
+      </section>
+    </div>
+  );
 }
 
 export default App;
